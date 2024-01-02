845a05f5eea5df2d1ab1ff7da81fda2495b9e645
joydepdhar <joydepdhar138@gmai.com>
2024-01-02 10:08:34 +0600

    Update 10

---
+++ StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/FeedBackController.cs
@@ -0,0 +1,107 @@
+﻿using BusinessLogicLayer.Services;
+using BusinessLogicLayer.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreManagementSystemAPI.Controllers
+{
+    public class FeedBackController : ApiController
+    {
+
+
+
+        [HttpGet]
+        [Route("api/feedback/get")]
+        public HttpResponseMessage feedback()
+        {
+            try
+            {
+                var data = FeedBackService.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("api/feedback/get/{id}")]
+        public HttpResponseMessage feedback(int id)
+        {
+            try
+            {
+                var data = FeedBackService.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpPost]
+        [Route("api/feedback/insert")]
+        public HttpResponseMessage Insert(FeedBackDTO cart)
+        {
+            try
+            {
+                var data = FeedBackService.Create(cart);
+                if (data)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Msg = "Inserted", Data = cart });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Msg = "Not Inserted", Data = cart });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpPost]
+        [Route("api/feedback/update")]
+        public HttpResponseMessage Update(FeedBackDTO cart)
+        {
+            try
+            {
+                var data = FeedBackService.Update(cart);
+                if (data)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Message = "Updated", Data = cart });
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = "Not Updated", Data = cart });
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("api/feedback/delete/{fid}")]
+        public HttpResponseMessage Delete(int fid)
+        {
+            try
+            {
+                var data = FeedBackService.Delete(fid);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+    }
+}
