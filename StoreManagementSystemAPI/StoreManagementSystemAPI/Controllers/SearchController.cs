2eb1ce0246f0b54aa6c7c5d036844d0f1ee27c84
joydepdhar <joydepdhar138@gmai.com>
2024-01-02 09:05:32 +0600

    Update-4

---
+++ StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/SearchController.cs
@@ -0,0 +1,74 @@
+﻿using BusinessLogicLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreManagementSystemAPI.Controllers
+{
+    public class SearchController : ApiController
+    {
+        [HttpGet]
+        [Route("api/user/search/{id}")]
+        public HttpResponseMessage ViewUser(int id)
+        {
+            try
+            {
+                var data = SearchService.ViewUser(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/users/search")]
+        public HttpResponseMessage ViewUsers()
+        {
+            try
+            {
+                var data = SearchService.ViewUser();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/product/search/{id}")]
+        public HttpResponseMessage ViewProduct(int id)
+        {
+            try
+            {
+                var data = SearchService.ViewProduct(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("api/products/search")]
+        public HttpResponseMessage ViewProducts()
+        {
+            try
+            {
+                var data = SearchService.ViewProduct();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+            }
+        }
+
+
+    }
+}
