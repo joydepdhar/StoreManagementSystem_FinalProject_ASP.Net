4ba4f0015ef1d798c562bece3512d8ce47ab6267
Your Name <abdulmuneemadnan77@gmail.com>
2024-01-02 08:53:01 +0600

    Project Update-2

---
+++ StoreManagementSystemAPI/StoreManagementSystemAPI/Controllers/StaffController.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLogicLayer.DTOs;
+using BusinessLogicLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StoreManagementSystemAPI.Controllers
+{
+    public class StaffController : ApiController
+    {
+        [HttpGet]
+        [Route("API/ViewProduct/{Id}")]
+        public HttpResponseMessage ViewProduct(int id)
+
+        {
+
+            try
+
+            {
+
+                var data = SearchService.ViewProduct(id);
+
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Message = ex.Message });
+
+            }
+
+        }
+
+    }
+
+}
